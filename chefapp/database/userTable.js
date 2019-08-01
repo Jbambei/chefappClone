850cc2e3bd900239a49da701c9e6881eed3b984d
Joel Bambei <joelbambei@gmail.com>
2019-08-01 18:33:19 -0400

    schema update

--- chefapp/database/userTable.js
+++ chefapp/database/userTable.js
@@ -9,42 +9,42 @@ module.exports = function(sequelize, datatypes) {
             type: Datatypes.STRING,
             required: true,
             len: [2, 50],
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userLastName: {
             type: Datatypes.STRING,
             required: true,
             len: [2, 50],
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userAllergies: {
             type: Datatypes.STRING,
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userPhone: {
             type: Datatypes.STRING,
-            isNumeric: true,          // will only allow numbers
+            isNumeric: true          // will only allow numbers
         },
         //Kitchen objects. More than likely more will be needed
         userStove: {
             type: Datatypes.STRING, //Making this string rather than Boolean so that we can pass in the type of grill
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userGrill: {
             type: Datatypes.STRING,
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userOven: {
             type: Datatypes.STRING,
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userMicrowave: {
             type: Datatypes.STRING,
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         },
         userSink: {
             type: Datatypes.STRING,
-            is: ["^[a-z]+$", "i"], // will only allow letters
+            is: ["^[a-z]+$", "i"] // will only allow letters
         }
     })
 }
