4b81bcfa0c36d2d007684195e234871fd017f64d
Joel Bambei <joelbambei@gmail.com>
2019-08-13 19:11:06 -0400

    latitude fix

--- client/src/utilities/latlonconverter.js
+++ client/src/utilities/latlonconverter.js
@@ -1,21 +1,5 @@
-​const NodeGeocoder = require('node-geocoder');
+​import NodeGeocoder from 'node-geocoder';
 ​
-const options = {
-    provider: 'google',
-    apiKey: 'AIzaSyArYUj_aKKGPm5FDl1dAf_CN_Ni62nkAMM',
-    formatter: null
-}
-​
-const geocoder = NodeGeocoder(options)
-const address = '2638 Windsor Hill Drive'
-​
-geocoder.geocode(address)
-    .then(function(res) {
-        console.log(res)
-    })
-    .catch(function(err) {
-        console.log(err)
-    })
 const options = {
     provider: 'google',
     apiKey: 'AIzaSyArYUj_aKKGPm5FDl1dAf_CN_Ni62nkAMM',
